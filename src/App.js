bc81fea06979528ce594662220f34ad4bd0f03f7
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-05-09 17:46:18 -0600

    feat: Will set up some hooks and pass in props to the components in main.

--- src/App.js
+++ src/App.js
@@ -7,12 +7,28 @@ function App() {
   const [movies, setMovies] = useState({movieMockData});
   const [focusDetails, setFocusDetails] = useState(false)
   const [focusId, setFocusId] = useState(0)
+  function updateId(id){
+    setFocusId(id)
+  };
+  /*This hook listens for a change in focusId (the invocation of updateId),
+  and toggles the focusDetails state. I did this so that we wouldn't have to write and pass in a updateFocusId function, 
+  just the updateId function. Updating the id to 0 changes focusDetails back to false */
+  useEffect(()=>{
+    if(focusId){
+      setFocusDetails(true)
+    } else {
+      setFocusDetails(false)
+    };
+  },[focusId]);
   return (
     <main>
       <header>Tomato</header>
-        <div class="center-view">
-        {!focusDetails ? <Movies /> : <Moviedetails />}
-
+        <div className="center-view">
+        {!focusDetails ? <Movies
+         movies = {movies} updateId = {updateId}
+        /> : <Moviedetails 
+        focusId = {focusId} updateId = {updateId}/*other details array should be passed in here as well, once made */
+        />}
         </div>
       <footer>---</footer>
     </main>
