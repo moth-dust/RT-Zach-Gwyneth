8f9788a634727fc45c640992cce47fe887f0f4d0
Zach Wolek <zach.wolek@gmail.com>
2024-05-09 16:28:34 -0500

    feat: Will create basic app framework with static elements.
    Co-authored-by: Gwyneth Patrick <gwynethpatrick235@gmail.com>
    Co-authored-by: Zach Wolek <zach.wolek@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,22 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <header>Tomato</header>
+        <div class="center-view">
+        {/* {!focusDetails ? <Movies /> : <Moviedetails />} */}
+        </div>
+      <footer>---</footer>
+    </main>
   );
 }
 
