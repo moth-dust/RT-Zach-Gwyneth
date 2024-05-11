12084c845717b324f906230ea94a8abde5fde2da
Zach Wolek <zach.wolek@gmail.com>
2024-05-10 22:44:42 -0500

    feat: creates a mapping of all movie data to pass to Card

--- src/Movies/Movies.js
+++ src/Movies/Movies.js
@@ -1,11 +1,26 @@
 import "./Movies.css"
 import Card from "../Card/Card";
 
-function Movies(movies){
-
-    // const movieCards = movies.map()
-
-
-    return (<Card />)
+function Movies({movies, updateId}){
+    console.log("movies", movies)
+    const movieCards = movies.movieMockData.movies.map(movie => {
+        return (
+            <Card 
+                id={movie.id}
+                key={movie.id}
+                poster_path={movie.poster_path}
+                backdrop_path={movie.backdrop_path}
+                title={movie.title}
+                average_rating={movie.average_rating}
+                release_date={movie.release_date}
+            />
+        )
+    })
+    console.log("Movie Cards: ", movieCards)
+    return (
+        <div className='movies-contaienr'>
+            {movieCards}
+        </div>
+    )
 }
 export default Movies;
