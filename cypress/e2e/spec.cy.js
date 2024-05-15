81aa54fe955b4542dc410ebea2059d81197b51f3
Zach Wolek <zach.wolek@gmail.com>
2024-05-15 17:57:22 -0500

    feat: Will add a fixture of mock data to the Cypress testing

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,19 +1,25 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
+
   })
 })
 
 describe('RT Testing', () => {
   beforeEach(() => {
+    cy.intercept("GET",'https://rancid-tomatillos.herokuapp.com/api/v2/movies', { 
+    fixture: 'movieData.json'
+    });
     cy.visit('http://localhost:3000/')
   })
-
+  
+  
   it('Should show the title of the application', () => {
     cy.get('#header').contains('Rancid Tomatillos')
   })
 
   it('Should see a collection of movies on page load.', () => {
-    cy.get('.movies-container').contains('div')
+    cy.get('.movies-container')
   })
+
 })
